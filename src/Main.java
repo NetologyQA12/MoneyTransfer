b819cd353300806f8e0cfb07df646b9f2eb000ae
NetologyQA12 <dvb90@bk.ru>
2020-12-25 19:52:39 +0700

    new file report.md

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 2_000_000_000;
+        int b = 700_000_000;
+        int c;
+        c = a + b;
+        System.out.println(c);
+    }
+}
+//int не может содержать число больше, чем 2_147_483_648
